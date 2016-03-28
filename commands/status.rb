aa13ab06fdd9392215c9c9681527d466b5ec3191
root <root@master.inf.puppetlabs.demo>
2016-03-28 20:54:55 +0000

    Added puppet status <node> to check node status

--- commands/status.rb
+++ commands/status.rb
@@ -1,9 +1,48 @@
 module PESlackBot
   module Commands
     class Status < SlackRubyBot::Commands::Base
-	match(/^status (?<node>\w*)\?$/i) do |client, data, match|
-	response = puppetdb.request('nodes',['=', 'certname', match[:node]])
-	client.say(channel: data.channel, text: "Last run for node #{match[:node]} was on #{response[:report_timestamp]}. Status was #{response[:latest_report_status]}. Report available <https://#{Puppet.settings[:ca_server]}/#/cm/report/#{response[:latest_report_hash]}/events|here>")
+      match(/^(?<bot>\w*)\sstatus\s(?<expression>.*)$/) do |client, data, match|
+        if (Puppet.settings[:confdir]) then
+          configfile = File.join([File.dirname(Puppet.settings[:confdir]), "peslackbot.yaml"])
+        else
+          configfile = "/etc/puppetlabs/puppet/peslackbot.yaml"
+        end
+        raise(ParseError, "PeSlackBot config file #{configfile} not readable") unless File.exist?(configfile)
+        config = YAML.load_file(configfile)
+        puppetdb = PuppetDB::Client.new({
+          :server => "https://#{config['puppetdbhost']}:8081/pdb/query",
+          :pem    => {
+            'key'     => config['hostprivkey'],
+            'cert'    => config['hostpubkey'],
+            'ca_file' => config['cakey']
+          }}, 4)
+        response = puppetdb.request('nodes',['=', 'certname', match[:expression]])
+        report_timestamp = response.data[0]['report_timestamp']
+        latest_report_status = response.data[0]['latest_report_status']
+        latest_report_hash = response.data[0]['latest_report_hash']
+        case latest_report_status
+          when 'changed'
+            color = '#4683A6'
+          when 'unchanged'
+            color = '#82C045'
+          when 'failed'
+            color = '#AD2927'
+          else
+            color = '#FFFFFF'
+          end
+        #client.say(channel: data.channel, text: "Last run for node #{match[:expression]} was on #{report_timestamp}. Status was #{latest_report_status}. Report available in https://#{config['puppetdbhost']}/#/cm/report/#{latest_report_hash}/events")
+        client.web_client.chat_postMessage(
+          channel: data.channel,
+          as_user: true,
+          attachments: [
+            {
+              fallback: "Last run for node #{match[:expression]} was on #{report_timestamp}. Status was #{latest_report_status}. Report available in https://#{config['puppetdbhost']}/#/cm/report/#{latest_report_hash}/events",
+              title: "#{match[:expression]} (#{latest_report_status})",
+              text: "Last run for node #{match[:expression]} was on #{report_timestamp}. Status was #{latest_report_status}. Report available in https://#{config['puppetdbhost']}/#/cm/report/#{latest_report_hash}/events",
+              color: color
+            }
+          ]
+        )
       end
     end
   end
