e250d81f659483cd03501eec72feae1cf45fa4ea
Nicolas Corrarello <nicolas@corrarello.com>
2016-03-30 12:47:45 +0000

    puppet job list now returns a list from the Orchestrator API. The limit parameter is introduced in the latest version so testing in 2015.3 will return a somewhat large list

--- commands/job.rb
+++ commands/job.rb
@@ -13,28 +13,45 @@ module PESlackBot
         tokenfile = "#{ENV['HOME']}/.puppetlabs/token"
         raise(ParseError, "No orchestrator token found on #{tokenfile}") unless File.exist?(tokenfile)
         if token=File.read(tokenfile)
+          orch = Net::HTTP.new(host, 8143)
+          orch.use_ssl = true
+          orch.verify_mode = OpenSSL::SSL::VERIFY_NONE
           #do whatever orchestration voodoo
           case match[:verb]
             when 'list'
-              client.web_client.chat_postMessage(
-                channel: data.channel,
-                as_user: true,
-                attachments: [
-                  {
-                  fallback: "List couldn't be displayed",
-                  title: "Job 1 by Joe",
-                  text: "completed on production on timestamp",
-                  color: '#82C045',
-                  },
-                  {
-                  fallback: "List couldn't be displayed",
-                  title: "Job 2 by Joe",
-                  text: "failed on production on timestamp",
-                  color: '#AD2927',
-                  },
-                ]
+              attachments = Array.new
+              request = Net::HTTP::Get.new("/orchestrator/v1/jobs?limit=5")
+              request.add_field("X-Authentication", token)
+              response = orch.request(request)
+              jobs = JSON.parse(response.body)
+              for job in jobs["items"] do
+                case job['state']
+                  when 'running'
+                    color = '#4683A6'
+                  when 'stopped'
+                    color = '#FFD801'
+                  when 'finished'
+                    color = '#82C045'
+                  when 'failed'
+                    color = '#AD2927'
+                  else
+                    color = '#FFFFFF'
+                  end
+                  if job['options']['noop']=='true'
+                    mode='in noop mode'
+                  end
+                  attachments.push(
+                    fallback: "Job #{job['name']} #{job['state']} on #{job['timestamp']} in #{job['environment']['name']} by #{job['owner']['login']} #{mode}",
+                    title: "Job #{job['name']} by #{job['owner']['login']}",
+                    text: "#{job['state']} on #{job['timestamp']} in #{job['environment']['name']} #{mode}",
+                    color: color
               )
-              #list jobs
+                client.web_client.chat_postMessage(
+                  channel: data.channel,
+                  as_user: true,
+                  attachments: attachments
+              )
+              end
             when 'run'
               #run a particular job. Requires at least environment as noun and an optional application as argument and noop flag as mode 
             when 'show'
