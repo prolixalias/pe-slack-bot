713a52c31584feefdaa54266d432a0eb7a117d2c
Nicolas Corrarello <ncorrare@gmail.com>
2016-04-07 16:59:14 +0100

    Initial refactor to remove the puppetdb-ruby gem

--- commands/status.rb
+++ commands/status.rb
@@ -9,40 +9,46 @@ module PESlackBot
         end
         raise(ParseError, "PeSlackBot config file #{configfile} not readable") unless File.exist?(configfile)
         config = YAML.load_file(configfile)
-        puppetdb = PuppetDB::Client.new({
-          :server => "https://#{config['puppetdbhost']}:8081/pdb/query",
-          :pem    => {
-            'key'     => config['hostprivkey'],
-            'cert'    => config['hostpubkey'],
-            'ca_file' => config['cakey']
-          }}, 4)
-        response = puppetdb.request('nodes',['=', 'certname', match[:expression]])
-        report_timestamp = response.data[0]['report_timestamp']
-        latest_report_status = response.data[0]['latest_report_status']
-        latest_report_hash = response.data[0]['latest_report_hash']
-        case latest_report_status
-          when 'changed'
-            color = '#4683A6'
-          when 'unchanged'
-            color = '#82C045'
-          when 'failed'
-            color = '#AD2927'
-          else
-            color = '#FFFFFF'
-          end
+	pdb = Net::HTTP.new(host, 8081)
+	pdb.use_ssl = true
+	pdb.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        privkeyraw = File.read(config['hostprivkey'])
+        certraw = File.read(config['hostpubkey'])
+	pdb.cert = OpenSSL::X509::Certificate.new(certraw)
+	pdb.key = OpenSSL::PKey::RSA.new(privkeyraw)
+	request = Net::HTTP::Get.new("/pdb/query/v4/nodes/#{match[:expression]}")
+	response = pdb.request(request)
+	if response.code == '200'
+	  host = JSON.parse(response.body)
+          report_timestamp = host['report_timestamp']
+          latest_report_status = host['latest_report_status']
+          latest_report_hash = host['latest_report_hash']
+          case latest_report_status
+            when 'changed'
+              color = '#4683A6'
+            when 'unchanged'
+              color = '#82C045'
+            when 'failed'
+              color = '#AD2927'
+            else
+              color = '#FFFFFF'
+            end
         #client.say(channel: data.channel, text: "Last run for node #{match[:expression]} was on #{report_timestamp}. Status was #{latest_report_status}. Report available in https://#{config['puppetdbhost']}/#/cm/report/#{latest_report_hash}/events")
-        client.web_client.chat_postMessage(
-          channel: data.channel,
-          as_user: true,
-          attachments: [
-            {
-              fallback: "Last run for node #{match[:expression]} was on #{report_timestamp}. Status was #{latest_report_status}. Report available in https://#{config['puppetdbhost']}/#/cm/report/#{latest_report_hash}/events",
-              title: "#{match[:expression]} (#{latest_report_status})",
-              text: "Last run for node #{match[:expression]} was on #{report_timestamp}. Status was #{latest_report_status}. Report available in https://#{config['puppetdbhost']}/#/cm/report/#{latest_report_hash}/events",
-              color: color
-            }
-          ]
-        )
+          client.web_client.chat_postMessage(
+            channel: data.channel,
+            as_user: true,
+            attachments: [
+              {
+                fallback: "Last run for node #{match[:expression]} was on #{report_timestamp}. Status was #{latest_report_status}. Report available in https://#{config['puppetdbhost']}/#/cm/report/#{latest_report_hash}/events",
+                title: "#{match[:expression]} (#{latest_report_status})",
+                text: "Last run for node #{match[:expression]} was on #{report_timestamp}. Status was #{latest_report_status}. Report available in https://#{config['puppetdbhost']}/#/cm/report/#{latest_report_hash}/events",
+                color: color
+              }
+            ]
+          )
+	else
+	  client.say(channel: data.channel, text: "Node wasn't found in PuppetDB")
+	end
       end
     end
   end
